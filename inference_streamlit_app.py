feb5e860ae24a67c64330e1bae8d20b2607bc613
Jimmy Wong <102245488+agi-singularity@users.noreply.github.com>
2024-04-09 16:27:34 -0700

    Update inference_streamlit_app.py

--- inference_streamlit_app.py
+++ inference_streamlit_app.py
@@ -28,12 +28,20 @@ with st.sidebar:
     
     df_action_sorted = df.sort_values(by="action_data", ascending=False)
     df_sorted = df.sort_values(by="activity_id", ascending=True)
-    activity_list = list(df_sorted.activity_id.unique()) #[::-1]
     
+    activity_list = list(df_sorted.activity_id.unique()) #[::-1]
     selected_activity = st.selectbox('Select an activity', activity_list)
     df_selected_activity = df[df.activity_id == selected_activity]
     df_selected_activity_sorted = df_selected_activity.sort_values(by="action_data", ascending=False)
 
+    action_list = list(df_sorted.action_data.unique())
+    selected_action = st.selectbox('Select an activity', action_list)
+    df_selected_action = df[df.action_data == selected_action]
+
+    wand_list = list(df_sorted.wand_identifier.unique())
+    selected_wand = st.selectbox('Select an activity', wand_list)
+    df_selected_wand = df[df.wand_identifier == selected_wand]
+    
     color_theme_list = ['blues', 'cividis', 'greens', 'inferno', 'magma', 'plasma', 'reds', 'rainbow', 'turbo', 'viridis']
     selected_color_theme = st.selectbox('Select a color theme', color_theme_list)
 
