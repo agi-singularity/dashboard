c6a97c6d40fe67754c470d369db49bb91318fd26
Jimmy Wong <102245488+agi-singularity@users.noreply.github.com>
2024-04-10 15:49:59 -0700

    Update inference_streamlit_app.py

--- inference_streamlit_app.py
+++ inference_streamlit_app.py
@@ -218,6 +218,7 @@ with col[1]:
     st.scatter_chart(data=chart_data, width=1000, height=1000, use_container_width=True)
 
 with col[2]:
+    st.markdown('#### Individual Wand Journey Activities')
     chart_data = df_selected_wand.groupby(by='activity_id').nunique()
     st.scatter_chart(data=chart_data, y=['action_data', 'session_id', 'event_id'], width=1000, height=1000, use_container_width=False)
     
