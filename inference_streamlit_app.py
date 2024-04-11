c018b37cd0001cfd16eb1ca1b283e44f048bc569
Jimmy Wong <102245488+agi-singularity@users.noreply.github.com>
2024-04-11 10:55:36 -0700

    Update inference_streamlit_app.py

--- inference_streamlit_app.py
+++ inference_streamlit_app.py
@@ -279,9 +279,10 @@ with tab4:
     def render_svg_example():
         svg_file = selected_svg #'data/opg1_red_rgb_alz_p2425.svg'
         import xml.dom.minidom
-
+        
+        x = ElementInclude.include(root)
         dom = xml.dom.minidom.parse(svg_file) # or 
-        #dom = xml.dom.minidom.parseString(treeX)
+        dom = xml.dom.minidom.parseString(x)
         svg = pretty_xml_as_string = dom.toprettyxml()
         
         # st.write('### SVG Input')
