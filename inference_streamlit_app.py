160e2dd7e0b49da95ca153cadc9b5675e56580b4
Jimmy Wong <102245488+agi-singularity@users.noreply.github.com>
2024-04-10 18:29:58 -0700

    Update inference_streamlit_app.py

--- inference_streamlit_app.py
+++ inference_streamlit_app.py
@@ -254,7 +254,7 @@ with tab4:
     def render_svg(svg):
         """Renders the given svg string."""
         b64 = base64.b64encode(svg.encode('utf-8')).decode("utf-8")
-        html = r'<img src="data:image/svg+xml+jpg;base64,%s"/>' % b64
+        html = r'<img src="data:image/svg+xml;base64,%s"/>' % b64
         st.write(html, unsafe_allow_html=True)
     
     def render_svg_example():
