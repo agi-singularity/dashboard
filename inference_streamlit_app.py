b36c16221548ab976da51b3f318ac38a9b620b19
Jimmy Wong <102245488+agi-singularity@users.noreply.github.com>
2024-04-11 11:09:18 -0700

    Update inference_streamlit_app.py

--- inference_streamlit_app.py
+++ inference_streamlit_app.py
@@ -270,7 +270,8 @@ with tab4:
         print(grandchild.attrib)
         count += 1
 
-    svg_str = ET.tostring(treex)
+    svg_sl = treex.tostringlist()
+    #svg_str = ET.tostring(treex)
     #treex.write('alz0405_blue_green.xml')
     
     def render_svg(svg):
